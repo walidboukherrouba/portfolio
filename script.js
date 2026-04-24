8f68698922ce387d80d3c70e2780994c9886144c
walidboukherrouba <boukherrouba.walid@gmail.com>
2026-04-24 18:26:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function showMessage() {
-  alert("Thanks for visiting my portfolio!");
-  window.open("https://mail.google.com/mail/?view=cm&to=boukherrouba.walid@gmail.com");
-}
-
-function changeColor() {
-  document.body.style.backgroundColor = "#dfe6e9";
-}
+console.log("Portfolio loaded successfully");
